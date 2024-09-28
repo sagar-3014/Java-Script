8f1152484edddacb1a59cfa61cbb153e30aa1548
SagarGanesh <sagarganesh3014@gmail.com>
2024-09-28 15:47:16 +0530

    Add files via upload

---
+++ reboot Web/emca.js
@@ -0,0 +1,5 @@
+function area(a,b){
+    var c=a*b
+    console.log(c)
+}
+area(10,20)
---
+++ reboot Web/todolist.js
@@ -0,0 +1,36 @@
+let todolist=[];
+function add(task)
+todolist.push(task)
+{
+    todolist.push(task)
+    console.log('added:"${task}"');
+}
+
+
+
+
+
+
+
+
+
+
+
+//var tasks=[]
+//function print(){
+  //  document.getElementById("task list").innerHTML=''
+    //for(i in tasks){
+      //  document.getElementById("task list").innerHTML+= tasks[i] + '<br>'
+    //}
+    //}
+    //function add(){
+      //  if(document.getElementById("task").value!=''){
+        //    tasks.push(document.getElementById("task").value)
+          //  document.getElementById("task").value=''
+            //print()
+        //}
+    //}
+    //function pop(){
+      //  tasks.shift()
+        //print()
+    //}
